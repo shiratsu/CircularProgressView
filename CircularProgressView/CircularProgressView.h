836c30d766b8aa44ebaada330e75d542a0f171fe
shiratsu2009 <shiratsu2009@gmail.com>
2021-09-26 23:25:39 +0900

    Initial Commit

---
+++ CircularProgressView/CircularProgressView.h
@@ -0,0 +1,18 @@
+//
+//  CircularProgressView.h
+//  CircularProgressView
+//
+//  Created by 平塚俊輔 on 2021/09/26.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CircularProgressView.
+FOUNDATION_EXPORT double CircularProgressViewVersionNumber;
+
+//! Project version string for CircularProgressView.
+FOUNDATION_EXPORT const unsigned char CircularProgressViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CircularProgressView/PublicHeader.h>
+
+
