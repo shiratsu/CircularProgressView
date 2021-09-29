fccfe44494e784eaf3b6a6523d3fb173c26bf8d7
shiratsu2009 <shiratsu2009@gmail.com>
2021-09-30 08:27:30 +0900

    change view set

--- CircularProgressViewExample/ViewController.swift
+++ CircularProgressViewExample/ViewController.swift
@@ -16,26 +16,16 @@ class ViewController: UIViewController {
 
     // MARK: - Properties -
 
-    @IBOutlet weak var containerView: UIView!
-    var circularView: CircularProgressView!
-    var duration: TimeInterval!
+    @IBOutlet weak var containerView: CircleBase!
+    
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        circularView = CircularProgressView()
-        circularView.frame = CGRect(x: 10, y: 10, width: 70, height: 70)
-        circularView.createCircularPath()
-        circularView.center = view.center
-        containerView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
-        view.addSubview(circularView)
+        
     }
     
-    @objc func handleTap() {
-        duration = 5    //Play with whatever value you want :]
-        circularView.progressAnimation(duration: duration)
-    }
+    
 
 
 }
